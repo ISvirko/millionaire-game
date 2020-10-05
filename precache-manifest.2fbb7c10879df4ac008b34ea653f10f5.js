6b1f4380b2e3c768a5b562c49236d8c550b7eb00
ISvirko <ISvirko@yahoo.com>
2020-10-05 17:12:57 +0300

    Updates

---
+++ precache-manifest.2fbb7c10879df4ac008b34ea653f10f5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453ec9c59e70e3f8060c4d16883ca000",
+    "url": "/millionaire-game/index.html"
+  },
+  {
+    "revision": "aa26749ee959580dd197",
+    "url": "/millionaire-game/static/js/2.88f7c04b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/millionaire-game/static/js/2.88f7c04b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3f329c9ed7acbddfb6",
+    "url": "/millionaire-game/static/js/main.a5b50985.chunk.js"
+  },
+  {
+    "revision": "5c2846d7d876c13c62ed",
+    "url": "/millionaire-game/static/js/runtime-main.23e937b9.js"
+  },
+  {
+    "revision": "d9cb525d71e4df9dc610ff32e85a3276",
+    "url": "/millionaire-game/static/media/background.d9cb525d.mp3"
+  },
+  {
+    "revision": "b00923136c9d4630ec067bc1260136a2",
+    "url": "/millionaire-game/static/media/final_answer.b0092313.mp3"
+  },
+  {
+    "revision": "fe5139ea7861cb299e1dff78418c78d0",
+    "url": "/millionaire-game/static/media/hand.fe5139ea.svg"
+  },
+  {
+    "revision": "a3c5c35e6434b79d75fef705a79301a8",
+    "url": "/millionaire-game/static/media/right.a3c5c35e.mp3"
+  },
+  {
+    "revision": "b1412542e669dfed09b312c5b601957c",
+    "url": "/millionaire-game/static/media/winning_theme.b1412542.mp3"
+  },
+  {
+    "revision": "b88830504cf3b62943834fc728a45324",
+    "url": "/millionaire-game/static/media/wrong.b8883050.mp3"
+  }
+]);
